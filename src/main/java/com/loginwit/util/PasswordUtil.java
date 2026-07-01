68505adaeb8e3423d2187f878043db00e20dbb8c
Angel1037 <angelalexistorresvargas4@gmail.com>
2026-07-01 01:15:41 -0600

    Update PasswordUtil.java

--- src/main/java/com/loginwit/util/PasswordUtil.java
+++ src/main/java/com/loginwit/util/PasswordUtil.java
@@ -1,1 +1,24 @@
+package com.loginwit.util;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+public final class PasswordUtil {
+
+    private PasswordUtil() {
+        // Evita crear instancias
+    }
+
+    /**
+     * Genera un hash seguro de una contraseña usando BCrypt.
+     */
+    public static String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(12));
+    }
+
+    /**
+     * Comprueba si una contraseña coincide con su hash.
+     */
+    public static boolean verify(String password, String hash) {
+        return BCrypt.checkpw(password, hash);
+    }
+}
